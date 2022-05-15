0c76135d0e6a427826ef09edd77f6204eb349d58
imankns70 <45065474+imankns70@users.noreply.github.com>
2022-05-15 22:58:08 +0430

    add mvc client

--- Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Controllers/MoviesController.cs
@@ -13,8 +13,7 @@ namespace Movies.API.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
-    //[Authorize("ClientIdPolicy")]
+    [Authorize("ClientIdPolicy")]
     public class MoviesController : ControllerBase
     {
 
