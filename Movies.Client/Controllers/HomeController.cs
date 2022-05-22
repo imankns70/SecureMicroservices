36f50103f002fbbfeffc42b67b3bb3c2520ea541
imankns70 <45065474+imankns70@users.noreply.github.com>
2022-05-22 08:25:01 +0330

    s

--- Movies.Client/Controllers/HomeController.cs
+++ Movies.Client/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace Movies.Client.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
